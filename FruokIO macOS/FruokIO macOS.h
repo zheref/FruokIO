101813dd4dc85629bf1eb24f797e905237cb64b4
Sergio Daniel Lozano <zheref@icloud.com>
2017-03-03 21:36:46 -0500

    Add macOS targets

---
+++ FruokIO macOS/FruokIO macOS.h
@@ -0,0 +1,19 @@
+//
+//  FruokIO macOS.h
+//  FruokIO macOS
+//
+//  Created by Sergio Daniel L. García on 3/3/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FruokIO macOS.
+FOUNDATION_EXPORT double FruokIO_macOSVersionNumber;
+
+//! Project version string for FruokIO macOS.
+FOUNDATION_EXPORT const unsigned char FruokIO_macOSVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruokIO_macOS/PublicHeader.h>
+
+
