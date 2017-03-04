69265e54191d3e4178ab7db6fd2c8becce86f6f3
Sergio Daniel Lozano <zheref@icloud.com>
2017-03-03 21:24:44 -0500

    Initial Commit

---
+++ FruokIO/FruokIO.h
@@ -0,0 +1,19 @@
+//
+//  FruokIO.h
+//  FruokIO
+//
+//  Created by Sergio Daniel L. García on 3/3/17.
+//  Copyright © 2017 Sergio Daniel L. García. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for FruokIO.
+FOUNDATION_EXPORT double FruokIOVersionNumber;
+
+//! Project version string for FruokIO.
+FOUNDATION_EXPORT const unsigned char FruokIOVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <FruokIO/PublicHeader.h>
+
+
