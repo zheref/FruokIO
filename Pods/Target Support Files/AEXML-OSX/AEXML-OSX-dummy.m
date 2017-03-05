eaec5914167aa51e3bb641621bdef72d70d33cc4
Sergio Daniel Lozano <zheref@icloud.com>
2017-03-04 19:20:51 -0500

    Pod install AEXML

---
+++ Pods/Target Support Files/AEXML-OSX/AEXML-OSX-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AEXML_OSX : NSObject
+@end
+@implementation PodsDummy_AEXML_OSX
+@end
---
+++ Pods/Target Support Files/AEXML-iOS/AEXML-iOS-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_AEXML_iOS : NSObject
+@end
+@implementation PodsDummy_AEXML_iOS
+@end
