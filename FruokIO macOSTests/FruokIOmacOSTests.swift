6ac4a94e4b4a42eaed434cc701626d5051175a5f
Sergio Daniel Lozano <zheref@icloud.com>
2017-03-05 22:09:47 -0500

    Testing file writing

--- FruokIO macOSTests/FruokIOmacOSTests.swift
+++ FruokIO macOSTests/FruokIOmacOSTests.swift
@@ -7,17 +7,29 @@
 //
 
 import XCTest
+
+import AEXML
+
 @testable import FruokIOmacOS
 
 class FruokIOmacOSTests: XCTestCase {
     
+    var xmldocument = AEXMLDocument()
+    
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        setupDocumentWithProject()
     }
     
-    func writeFileWithAnimal() {
-        
+    func setupDocumentWithProject() {
+        let attributes = ["xmlns:xsi": "http://www.w3.org/2001/XMLSchema-instance",
+                          "xmlns:xsd": "http://www.w3.org/2001/XMLSchema"]
+        let project = xmldocument.addChild(name: "frk:project", attributes: attributes)
+        project.addChild(name: "frk:freelance", value: "Sergio Daniel Lozano Garcia",
+                         attributes: ["frk:title": "Software Engineer"])
+        project.addChild(name: "frk:client", value: "Universidad de Ibague",
+                         attributes: ["frk:socialid": "988717254-2"])
     }
     
     override func tearDown() {
@@ -28,6 +40,13 @@ class FruokIOmacOSTests: XCTestCase {
     func testExample() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
+        let document = Document(withContent: xmldocument,
+                                andUrl: URL(fileURLWithPath: "/Users/zheref/test-project"))
+        document.save() {
+            // do something
+        }
+        
+        XCTAssert(true)
     }
     
     func testPerformanceExample() {
